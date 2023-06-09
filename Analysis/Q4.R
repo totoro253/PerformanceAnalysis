55396ae266cd3e074bf52dff617a1242b8ff350e
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-06-09 15:31:45 +0200

    Create Q4.R

---
+++ Analysis/Q4.R
@@ -0,0 +1,11 @@
+#DOTCOM Bubble
+
+##  What is the dotcom bubble?
+The dot-com bubble was a stock market bubble 
+fueled by highly speculative investments in 
+internet-based businesses during the bull market
+from 1995 to 2000. It saw the value of equity markets 
+grow dramatically, with the technology-dominated Nasdaq
+index rising five-fold during that period.
+
+#
