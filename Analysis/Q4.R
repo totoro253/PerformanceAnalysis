98352eea5108e1744563e53d870e01bcd870e5fb
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-06-09 16:04:01 +0200

    added adjusted year 2000/1995 and a overall plot open. and. adjusted
    Added a introduction and a plot with the opening prices.  and closing prices during the entire period. of. 1995 until 2000. and a seperated plot. for just the year.  1995. and the year 2000

--- Analysis/Q4.R
+++ Analysis/Q4.R
@@ -32,9 +32,44 @@ stock_prices_1995 <- c("MSFT","GE") %>%
 stock_prices_1995
 
 ## How did the adjusted price changed compaired to the opening price. in 1995?
-ggplot(stock_prices_1995, aes(date, open,  color=close)) +
+ggplot(stock_prices_1995, aes(date, adjusted,  color=symbol)) +
   geom_line()
 
 
 
 ## What was the stock in 2000?
+
+stock_prices_2000 <- c("MSFT","GE") %>%
+  tq_get(get = "stock.prices",
+         from = "2000-01-01",
+         to = "2000-12-01") %>%
+  group_by(symbol)
+
+stock_prices_2000
+
+#Compare
+
+stock_adjusted_2000 <-ggplot(stock_prices_2000, aes(date, adjusted,  color=symbol)) +
+  geom_line()
+
+stock_adjusted_1995<-ggplot(stock_prices_1995, aes(date, adjusted,  color=symbol)) +
+  geom_line()
+
+stock_prices_1995
+
+stock_adjusted_2000
+
+
+##. What was the overall adjusting price during the period of 1995 until 2000?
+stock_prices_9520 <- c("MSFT","GE") %>%
+  tq_get(get = "stock.prices",
+         from = "1995-01-01",
+         to = "2000-12-01") %>%
+  group_by(symbol)
+
+
+open_19952000 <-ggplot(stock_prices_9520, aes(date, open,  color=symbol)) +
+  geom_line()
+
+adjusted_1995200 <-ggplot(stock_prices_9520, aes(date, adjusted,  color=symbol)) +
+  geom_line()
